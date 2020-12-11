e888385cd745b4268f3f706712647087c8b0ee02
azizzade <azizzade1992@gmail.com>
2020-12-11 13:53:34 +0400

    !

--- Js/statistic.js
+++ Js/statistic.js
@@ -1,16 +1,21 @@
-var chart = new Highcharts.Chart({
-    chart: {
-      renderTo: 'container',
-      marginBottom: 80
-    },
+
+let chart_line = document.getElementById("#chart_line");
+
+let myChart = echarts.init(chart_line);
+
+let option = {
     xAxis: {
-      categories: ['10:00', '11:00', '12:00', '13:00', '14::00', '15:00', '16:00', '17:00', '18:00'],
-      labels: {
-        rotation: 90
-      }
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     },
-  
+    yAxis: {
+        type: 'value'
+    },
     series: [{
-      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]        
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
     }]
-  });
+};
+
+myChart.setOption(option);
