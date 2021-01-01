0bc3ae3060c755846b8b0ff9e680725bc07b471c
azizzade <azizea@code.edu.az>
2021-01-02 02:19:53 +0400

    edit pages

--- Js/script.js
+++ Js/script.js
@@ -162,52 +162,52 @@ play_icon.addEventListener("click", function(){
 
 
 
-let new_operator_btn = document.querySelector(".new_button");
-let close_btn = document.querySelector(".close_button");
-let modal = document.querySelector(".modal");
+// let new_operator_btn = document.querySelector(".new_button");
+// let close_btn = document.querySelector(".close_button");
+// let modal = document.querySelector(".modal");
 
 
-new_operator_btn.addEventListener("click", function() {
-    modal.style.display = "block";
-});
+// new_operator_btn.addEventListener("click", function() {
+//     modal.style.display = "block";
+// });
 
-close_btn.addEventListener("click", function() {
-    modal.style.display = 'none';
-});
+// close_btn.addEventListener("click", function() {
+//     modal.style.display = 'none';
+// });
 
-window.addEventListener("click", function() {
-    if(event.target == modal){
-        modal.style.display = 'none';
-    }
-})
+// window.addEventListener("click", function() {
+//     if(event.target == modal){
+//         modal.style.display = 'none';
+//     }
+// })
 
 
-let operator_edit_button = document.querySelector(".operator_edit_button");
-let edit_delete_btn = document.querySelectorAll(".edit_delete_btn");
+// let operator_edit_button = document.querySelector(".operator_edit_button");
+// let edit_delete_btn = document.querySelectorAll(".edit_delete_btn");
 
-operator_edit_button.addEventListener('click', function() {
+// operator_edit_button.addEventListener('click', function() {
 
-  for (let index = 0; index < edit_delete_btn.length; index++) {
-   let element = edit_delete_btn[index];
-   if (element.style.display == "none"){
-    element.style.display = "block";
-    element.style.display = "flex";
-  }else {
-    element.style.display = "none";
-  }
-  }
-})
+//   for (let index = 0; index < edit_delete_btn.length; index++) {
+//    let element = edit_delete_btn[index];
+//    if (element.style.display == "none"){
+//     element.style.display = "block";
+//     element.style.display = "flex";
+//   }else {
+//     element.style.display = "none";
+//   }
+//   }
+// })
 
-let delete_btn = document.querySelectorAll(".delete_btn");
-let modal_delete = document.querySelector(".modal_delete");
-let chanel_btn =document.getElementById("chanel_btn");
+// let delete_btn = document.querySelectorAll(".delete_btn");
+// let modal_delete = document.querySelector(".modal_delete");
+// let chanel_btn =document.getElementById("chanel_btn");
 
-for (let index = 0; index < delete_btn.length; index++) {
-  delete_btn[index].addEventListener('click', function(){
-    modal_delete.style.display = "block";
-  })
-}
+// for (let index = 0; index < delete_btn.length; index++) {
+//   delete_btn[index].addEventListener('click', function(){
+//     modal_delete.style.display = "block";
+//   })
+// }
 
-chanel_btn.addEventListener('click', function(){
-  modal_delete.style.display = "none";
-})
+// chanel_btn.addEventListener('click', function(){
+//   modal_delete.style.display = "none";
+// })
